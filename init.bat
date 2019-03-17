3262b850e2eb8eea5cd7587510e1b5b4d3872a77
Simon Hölscher <simon.hoelscher@posteo.de>
2019-03-17 20:58:53 +0100

    setting loglevel and starting daemons in separate windows

--- init.bat
+++
@@ -1,7 +0,0 @@
-FOR /L %%A IN (1,1,30) DO (
-  set IPFS_PATH=~/.ipfs%%A
-  ipfs init
-)
-
-cd %HOMEPATH%
-ipfs-sybil-changeconfigs 30
--- loglevel.bat
+++ loglevel.bat
@@ -1,4 +1,4 @@
 FOR /L %%A IN (1,1,10) DO (
   set IPFS_PATH=~/.ipfsSybil%%A
-  START cmd /K ipfs daemon
+  START cmd /K ipfs log level dht info
 )
